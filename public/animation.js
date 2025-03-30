17d0e8b0c89ce47a675aec5c85111e4442d924d5
rogo! <r480github@gmail.com>
2025-03-30 19:45:07 -0400

    add games

---
+++ public/animation.js
@@ -0,0 +1,55 @@
+gsap.to(".h1one", {
+  opacity: 1,
+  x: 40,
+  duration: 1.2,
+  ease: "expo.inOut",
+});
+gsap.to(".h1two", {
+  opacity: 1,
+  x: 25,
+  duration: 1.2,
+  ease: "expo.inOut",
+  delay: 0.1,
+});
+gsap.to(".h1three", {
+  opacity: 1,
+  x: 10,
+  delay: 0.2,
+  duration: 1.2,
+  ease: "expo.inOut",
+});
+
+gsap.to(".h1four", {
+  opacity: 1,
+  x: -10,
+  duration: 1.2,
+  ease: "expo.inOut",
+});
+gsap.to(".h1five", {
+  opacity: 1,
+  x: -25,
+  duration: 1.2,
+  ease: "expo.inOut",
+  delay: 0.1,
+});
+gsap.to(".h1six", {
+  opacity: 1,
+  x: -40,
+  delay: 0.2,
+  duration: 1.2,
+  ease: "expo.inOut",
+});
+gsap.fromTo(
+  ".navbar__item",
+  {
+    opacity: 0,
+    x: -50,
+  },
+  {
+    opacity: 1,
+    x: 0,
+    duration: 1.2,
+    delay: 0,
+    ease: "expo.inOut",
+  }
+);
---
+++ public/g/g.js
@@ -0,0 +1,36 @@
+fetch("g.json")
+  .then((response) => response.json())
+  .then((games) => {
+    const appsContainer = document.querySelector(".games");
+
+    games.forEach((game, index) => {
+      const gameElement = document.createElement("div");
+      gameElement.className = "game";
+      gameElement.dataset.index = index; 
+
+      gameElement.innerHTML = `
+        <img src="/global/img/${game.image}" alt="${game.name}" class="cards">
+        <h3 class="cardname">${game.name}</h3>
+        <h3 class="cardgenre">${game.genre}</h3>
+      `;
+
+      gameElement.addEventListener("click", async () => {
+        localStorage.setItem('previous', window.location.href);
+        console.log('previous page is ' + window.location.href);
+        if (game.url) {
+          var ute = game.url;
+          if (localStorage.getItem("proxy") == "uv") {
+            ute = __uv$config.prefix + __uv$config.encodeUrl(ute);
+            localStorage.setItem("url", ute);
+            window.location.href = "/search.html";
+          } 
+        } else if (game.file) {
+          var fil = game.file;
+          localStorage.setItem('gameload', fil);
+          window.location.href = "/gameload.html";
+        }
+      });
+
+      appsContainer.appendChild(gameElement);
+    });
+  });
