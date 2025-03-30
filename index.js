53815cf766d7c0b9d23c3e4b953b7f7aebf80ce3
rogo! <r480github@gmail.com>
2025-03-30 04:49:48 -0400

    basics

---
+++ index.js
@@ -0,0 +1,22 @@
+import { ChemicalServer } from "chemicaljs";
+import express from "express";
+
+const [app, listen] = new ChemicalServer();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public", {
+    index: "index.html",
+    extensions: ["html"]
+}));
+
+app.serveChemical();
+
+listen(port, () => {
+    console.log("░██████╗░░█████╗░██╗░░░░░░█████╗░██╗░░██╗██╗░░░██╗");
+    console.log("██╔════╝░██╔══██╗██║░░░░░██╔══██╗╚██╗██╔╝╚██╗░██╔╝");
+    console.log("██║░░╚██╗██╔══██║██║░░░░░██╔══██║░██╔██╗░░░╚██╔╝░░");
+    console.log("╚██████╔╝██║░░██║███████╗██║░░██║██╔╝╚██╗░░░██║░░░");
+    console.log("░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░");
+    console.log("is listening on:");
+    console.log(`http://localhost:${port}`);
+});
