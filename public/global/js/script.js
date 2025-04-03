28d46774d29852822ad81a3b7a690867ae6cdbe2
rogo! <119364101+r480github@users.noreply.github.com>
2025-04-03 18:14:48 +0000

    proxy selector

--- public/global/js/script.js
+++ public/global/js/script.js
@@ -1,12 +1,32 @@
-document.getElementById("search").addEventListener("keypress", function(event) {
+document.getElementById("search").addEventListener("keypress", function (event) {
   if (event.key === "Enter") { // Check if the "Enter" key was pressed
     event.preventDefault(); // Optional: Prevent default behavior, if needed
     document.getElementById("web").style.visibility = "visible";
     document.getElementById("controls").style.visibility = "visible";
   }
 });
-document.querySelector("select").addEventListener("change", function() {
+document.querySelector("select").addEventListener("change", function () {
   const selectedValue = this.value; // Get the selected option's value
   const searchInput = document.querySelector("#search"); // Get the input element
   searchInput.setAttribute("data-service", selectedValue); // Set the data-service attribute
 });
+
+
+/*
+// Get the dropdown element
+const dropdown = document.getElementById('myDropdown');
+
+// On page load, set the selected value based on localStorage
+window.onload = function () {
+  const savedValue = localStorage.getItem('dropdownValue');
+  if (savedValue) {
+    dropdown.value = savedValue;
+  }
+};
+
+// Save the selected value whenever it changes
+dropdown.addEventListener('change', function () {
+  localStorage.setItem('dropdownValue', dropdown.value);
+});
+
+*/
