9def80cec0b234c75ecc66afb54c71769a5275f9
rogo! <r480github@gmail.com>
2025-04-08 22:36:19 -0400

    settings!

--- public/settings/setting.js
+++ public/settings/setting.js
@@ -1,36 +1,107 @@
-fetch("g.json")
-  .then((response) => response.json())
-  .then((games) => {
-    const appsContainer = document.querySelector(".games");
+try {
+  var analyticsScript = document.createElement("script");
+  analyticsScript.src =
+    "https://www.googletagmanager.com/gtag/js?id=G-PYBNSB93F8";
+  analyticsScript.async = true;
+  document.head.appendChild(analyticsScript);
+  var analyticsScript2 = document.createElement("script");
+  analyticsScript2.innerHTML = `
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    games.forEach((game, index) => {
-      const gameElement = document.createElement("div");
-      gameElement.className = "game";
-      gameElement.dataset.index = index; 
+  gtag('config', 'G-PYBNSB93F8');
+`;
+  document.head.appendChild(analyticsScript2);
+} catch {}
 
-      gameElement.innerHTML = `
-        <img src="/global/img/${game.image}" alt="${game.name}" class="cards">
-        <h3 class="cardname">${game.name}</h3>
-        <h3 class="cardgenre">${game.genre}</h3>
-      `;
+let inIframe;
+try {
+  inIframe = window.self !== window.top;
+} catch (e) {
+  inIframe = true;
+}
+const educationalSites = ["https://classroom.google.com"];
 
-      gameElement.addEventListener("click", async () => {
-        localStorage.setItem('previous', window.location.href);
-        console.log('previous page is ' + window.location.href);
-        if (game.url) {
-          var ute = game.url;
-          document.getElementById("search").value = ute;
-          document.getElementById("submitbutton").click();
-          document.getElementById("web").style.visibility = "visible";
-          document.getElementById("controls").style.visibility = "visible";
-      
-        } else if (game.file) {
-          var fil = game.file;
-          localStorage.setItem('gameload', fil);
-          window.location.href = "/load";
+function ABCloak(redirectToEducationalSite) {
+  try {
+    if (!inIframe) {
+      const popup = open("about:blank", "_blank");
+      if (popup) {
+        const doc = popup.document;
+        const iframe = doc.createElement("iframe");
+        const style = iframe.style;
+        const link = doc.createElement("link");
+
+        var name = "Google Classroom";
+        var icon =
+          "https://ssl.gstatic.com/classroom/favicon.png";
+        if (localStorage.getItem("cloakTitle") !== null) {
+          name = localStorage.getItem("cloakTitle");
+          icon = localStorage.getItem("cloakIcon");
         }
-      });
+        doc.title = name;
+        link.rel = "icon";
+        link.href = icon;
 
-      appsContainer.appendChild(gameElement);
-    });
-  });
+        iframe.src = location.href;
+        style.border = style.outline = "none";
+        style.width = style.height = "100%";
+        style.overflow = "hidden";
+        doc.body.style.margin = style.margin = 0;
+        style.padding = 0;
+        doc.head.appendChild(link);
+        doc.body.appendChild(iframe);
+        doc.URLBeforeCloak = location.href;
+
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.innerHTML =
+          `
+          window.onmessage = function (e) {
+            if (e.data == 'cancelABCloak') {
+              location.replace("` +
+          location.href +
+          `");
+            } else {
+              try {
+                var msg = JSON.parse(e.data);
+                if (msg.msg === "changeCloak") {
+                  document.title = msg.title
+                  let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                  link.type = 'image/x-icon';
+                  link.rel = 'shortcut icon';
+                  link.href = msg.icon;
+                  document.getElementsByTagName('head')[0].appendChild(link);
+                }
+              } catch { }
+            }
+          };`;
+        doc.body.append(script);
+
+        if (redirectToEducationalSite)
+          location.replace(
+            educationalSites[
+              Math.floor(Math.random() * educationalSites.length)
+            ]
+          );
+      }
+    }
+  } catch {
+    ABCloak(true);
+  }
+}
+
+if (localStorage.getItem("autoAB") == "true") {
+  ABCloak(true);
+}
+function checkPopupsBlocked() {
+  const testPopup = window.open('', '_blank');
+
+  if (!testPopup || testPopup.closed || typeof testPopup.closed === 'undefined') {
+    alert('Popups needs to be enabled');
+  } else {
+    testPopup.close();
+  }
+}
+
