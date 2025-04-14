2c8c5868bdf267c158d1f99c884513311f072622
rogo! <r480github@gmail.com>
2025-04-14 19:46:59 -0400

    final bug fix

--- public/global/js/swup.js
+++ public/global/js/swup.js
@@ -1,99 +1,106 @@
 const swup = new Swup();
-function initGsapAnimations() {
+function initsettingjs() {
+  // Check if the autoABToggle element exists
+  const autoABToggle = document.getElementById("autoABToggle");
+  if (!autoABToggle) {
+    console.warn("autoABToggle element not found. Stopping script.");
+    return; // Exit the script
+  }
 
-    let inIframe;
+  // Initialize localStorage value if not already set
+  if (localStorage.getItem("autoAB") === null) {
+    localStorage.setItem("autoAB", "false");
+  }
+
+  // Set the checkbox based on localStorage value
+  autoABToggle.checked = localStorage.getItem("autoAB") === "true";
+
+  // Update localStorage value when the checkbox is changed
+  autoABToggle.addEventListener("change", function () {
+    localStorage.setItem("autoAB", this.checked);
+    if (this.checked) {
+      checkPopupsBlocked();
+      location.reload();
+      console.log("AB Updated!");
+    }
+  });
+  let inIframe;
+  try {
+    inIframe = window.self !== window.top;
+  } catch (e) {
+    inIframe = true;
+  }
+  const educationalSites = ["https://classroom.google.com"];
+
+  function ABCloak(redirectToEducationalSite) {
     try {
-      inIframe = window.self !== window.top;
-    } catch (e) {
-      inIframe = true;
-    }
-    const educationalSites = ["https://classroom.google.com"];
-  
-    function ABCloak(redirectToEducationalSite) {
-      try {
-        if (!inIframe) {
-          const popup = open("about:blank", "_blank");
-          if (popup) {
-            const doc = popup.document;
-            const iframe = doc.createElement("iframe");
-            const style = iframe.style;
-            const link = doc.createElement("link");
-  
-            var name = "Google Classroom";
-            var icon = "https://ssl.gstatic.com/classroom/favicon.png";
-            if (localStorage.getItem("cloakTitle") !== null) {
-              name = localStorage.getItem("cloakTitle");
-              icon = localStorage.getItem("cloakIcon");
-            }
-            doc.title = name;
-            link.rel = "icon";
-            link.href = icon;
-  
-            iframe.src = location.href;
-            style.border = style.outline = "none";
-            style.width = style.height = "100%";
-            style.overflow = "hidden";
-            doc.body.style.margin = style.margin = 0;
-            style.padding = 0;
-            doc.head.appendChild(link);
-            doc.body.appendChild(iframe);
-            doc.URLBeforeCloak = location.href;
-  
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.innerHTML =
-              `
-                  window.onmessage = function (e) {
-                    if (e.data == 'cancelABCloak') {
-                      location.replace("` +
-              location.href +
-              `");
-                    } else {
-                      try {
-                        var msg = JSON.parse(e.data);
-                        if (msg.msg === "changeCloak") {
-                          document.title = msg.title
-                          let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-                          link.type = 'image/x-icon';
-                          link.rel = 'shortcut icon';
-                          link.href = msg.icon;
-                          document.getElementsByTagName('head')[0].appendChild(link);
-                        }
-                      } catch { }
-                    }
-                  };`;
-            doc.body.append(script);
-  
-            if (redirectToEducationalSite)
-              location.replace(
-                educationalSites[
-                  Math.floor(Math.random() * educationalSites.length)
-                ]
-              );
+      if (!inIframe) {
+        const popup = open("about:blank", "_blank");
+        if (popup) {
+          const doc = popup.document;
+          const iframe = doc.createElement("iframe");
+          const style = iframe.style;
+          const link = doc.createElement("link");
+
+          var name = "Google Classroom";
+          var icon = "https://ssl.gstatic.com/classroom/favicon.png";
+          if (localStorage.getItem("cloakTitle") !== null) {
+            name = localStorage.getItem("cloakTitle");
+            icon = localStorage.getItem("cloakIcon");
           }
+          doc.title = name;
+          link.rel = "icon";
+          link.href = icon;
+
+          iframe.src = location.href;
+          style.border = style.outline = "none";
+          style.width = style.height = "100%";
+          style.overflow = "hidden";
+          doc.body.style.margin = style.margin = 0;
+          style.padding = 0;
+          doc.head.appendChild(link);
+          doc.body.appendChild(iframe);
+          doc.URLBeforeCloak = location.href;
+
+          var script = document.createElement("script");
+          script.type = "text/javascript";
+          script.innerHTML =
+            `
+                    window.onmessage = function (e) {
+                      if (e.data == 'cancelABCloak') {
+                        location.replace("` +
+            location.href +
+            `");
+                      } else {
+                        try {
+                          var msg = JSON.parse(e.data);
+                          if (msg.msg === "changeCloak") {
+                            document.title = msg.title
+                            let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                            link.type = 'image/x-icon';
+                            link.rel = 'shortcut icon';
+                            link.href = msg.icon;
+                            document.getElementsByTagName('head')[0].appendChild(link);
+                          }
+                        } catch { }
+                      }
+                    };`;
+          doc.body.append(script);
+
+          if (redirectToEducationalSite)
+            location.replace(
+              educationalSites[
+                Math.floor(Math.random() * educationalSites.length)
+              ]
+            );
         }
-      } catch {
-        ABCloak(true);
       }
-    }
-  
-    if (localStorage.getItem("autoAB") == "true") {
+    } catch {
       ABCloak(true);
     }
-    function checkPopupsBlocked() {
-      const testPopup = window.open("", "_blank");
-  
-      if (
-        !testPopup ||
-        testPopup.closed ||
-        typeof testPopup.closed === "undefined"
-      ) {
-        alert("Popups needs to be enabled");
-      } else {
-        testPopup.close();
-      }
-    }
-  
+  }
+}
+function initGsapAnimations() {
   //gsap stuff
   gsap.fromTo(
     ".game1",
@@ -562,10 +569,10 @@ function initGsapAnimations() {
 }
 
 swup.hooks.on("page:view", () => {
+  initsettingjs()
   initGsapAnimations();
   const ionicons = document.querySelectorAll("ion-icon");
   ionicons.forEach((icon) => {
     console.log("Ionicon reinitialized:", icon);
   });
-  
 });
